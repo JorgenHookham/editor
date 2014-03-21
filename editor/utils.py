dd47611ac80501e751c636d202aeddacddb7b838
Jean-Marc Skopek <jskopek@gmail.com>
2014-03-21 00:27:41 -0700

    Moved utils to separate utils file

---
+++ editor/utils.py
@@ -0,0 +1,54 @@
+from collections import Counter
+from dateutil import parser
+from django.core.urlresolvers import reverse
+from django.conf import settings
+from django.utils import simplejson
+import dropbox
+
+def analyze_question(client, question):
+    counts = Counter()
+    for file_metadata in get_app_folder(client)['contents']:
+        content = get_file_content(client, file_metadata)
+        content = simplejson.loads(content)
+        for snapshot in content['snapshots']:
+            for response in snapshot['responses']:
+                if response['questionPrompt'].lower() == question.lower():
+                    for answered_option in response['answeredOptions']:
+                        counts[answered_option] += 1
+    return counts
+
+def build_pie_chart_data(data_dict):
+    colors = ["D71E15","E76517","FEB01B","FECB1B","FEDF74","FF5E49","FAAE0D","FDFBC8"]
+    pie_items = []
+    for index, key in enumerate(data_dict.keys()):
+        pie_items.append({'label':key,'value':data_dict[key],'color':colors[index]})
+    return {'item':pie_items}
+
+def get_app_folder(client):
+    try:
+        return client.metadata('/Apps/Reporter-App/')
+    except dropbox.rest.ErrorResponse:
+        raise Exception('Reporter app not linked on Dropbox')
+
+       
+def get_file_content(client, file_metadata):
+    f, metadata = client.get_file_and_metadata(file_metadata['path'])
+    return f.read()
+
+def get_flow(request):
+    protocol = 'https' if request.is_secure() else 'http'
+    callback_url = '%s://%s%s' % (protocol, request.META['HTTP_HOST'], reverse('authorize_callback'))
+    print settings.DROPBOX_API_SECRET
+    flow = dropbox.client.DropboxOAuth2Flow(settings.DROPBOX_API_KEY, settings.DROPBOX_API_SECRET, callback_url, request.session, 'dropbox-auth-csrf-token')
+    return flow
+
+# Deprecated
+def get_last_file(folder):
+    last_file = None
+    for file_metadata in folder['contents']:
+        if not last_file:
+            last_file = file_metadata
+        elif parser.parse(file_metadata['modified']) > parser.parse(last_file['modified']):
+            last_file = file_metadata
+    return last_file
+ 
--- editor/views.py
+++ editor/views.py
@@ -1,12 +1,12 @@
-from collections import Counter
-from dateutil import parser
 from django.http import HttpResponse
 from django.http import HttpResponseRedirect
-from django.core.urlresolvers import reverse
 from django.core.cache import cache
-from django.conf import settings
 from django.utils import simplejson
 from django.shortcuts import redirect
+from editor.utils import analyze_question
+from editor.utils import build_pie_chart_data
+from editor.utils import get_app_folder
+from editor.utils import get_flow
 import dropbox
 import random
 import string
@@ -37,9 +37,7 @@ def authenticated_home(request, key):
         return redirect('welcome')
     else:
         client = dropbox.client.DropboxClient(access_token)
-        file_data = get_last_reporter_export(client)
-        
-        return HttpResponse(file_data, content_type='text/plain')
+        return HttpResponse('hello', content_type='text/plain')
 
 def pie_chart(request, key):
     question = request.GET['question']
@@ -48,58 +46,4 @@ def pie_chart(request, key):
     data = analyze_question(client, question)
     pie_data = build_pie_chart_data(data)
     return HttpResponse(simplejson.dumps(pie_data), content_type='application/json')
-
-
-#### UTILS #####
-def analyze_question(client, question):
-    counts = Counter()
-    for file_metadata in get_app_folder(client)['contents']:
-        content = get_file_content(client, file_metadata)
-        content = simplejson.loads(content)
-        for snapshot in content['snapshots']:
-            for response in snapshot['responses']:
-                if response['questionPrompt'].lower() == question.lower():
-                    for answered_option in response['answeredOptions']:
-                        counts[answered_option] += 1
-    return counts
-
-def build_pie_chart_data(data_dict):
-    colors = ["D71E15","E76517","FEB01B","FECB1B","FEDF74","FF5E49","FAAE0D","FDFBC8"]
-    pie_items = []
-    for index, key in enumerate(data_dict.keys()):
-        pie_items.append({'label':key,'value':data_dict[key],'color':colors[index]})
-    return {'item':pie_items}
-
-def get_last_reporter_export(client):
-    app_folder = get_app_folder(client)
-    last_response = get_last_file(app_folder)
-    return get_file_content(client, last_response)
-
-
-def get_app_folder(client):
-    try:
-        return client.metadata('/Apps/Reporter-App/')
-    except dropbox.rest.ErrorResponse:
-        raise Exception('Reporter app not linked on Dropbox')
-
-def get_last_file(folder):
-    last_file = None
-    for file_metadata in folder['contents']:
-        if not last_file:
-            last_file = file_metadata
-        elif parser.parse(file_metadata['modified']) > parser.parse(last_file['modified']):
-            last_file = file_metadata
-    return last_file
-        
-def get_file_content(client, file_metadata):
-    f, metadata = client.get_file_and_metadata(file_metadata['path'])
-    return f.read()
-
-def get_flow(request):
-    protocol = 'https' if request.is_secure() else 'http'
-    callback_url = '%s://%s%s' % (protocol, request.META['HTTP_HOST'], reverse('authorize_callback'))
-    print settings.DROPBOX_API_SECRET
-    flow = dropbox.client.DropboxOAuth2Flow(settings.DROPBOX_API_KEY, settings.DROPBOX_API_SECRET, callback_url, request.session, 'dropbox-auth-csrf-token')
-    return flow
-
 
